6a9c0ac0e2f2838d960a7ed03b15bc08c98f19f8
Andrii Hromanchuk <73118147+TrueMage@users.noreply.github.com>
2023-10-23 14:28:57 +0200

    added class board

---
+++ Board.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    internal class Board
+    {
+        private int[,] _board = new int[9, 9];
+
+        public bool InsertValue(int row, int col, int value)
+        {
+            bool result = IsValidMove(row, col, value);
+            if(result) _board[row, col] = value;
+            return result;
+        }
+
+        public bool IsValidMove(int row, int col, int value)
+        {
+            // Проверка строки и столбца
+            for (int i = 0; i < 9; i++)
+            {
+                if (_board[row, i] == value || _board[i, col] == value)
+                {
+                    return false;
+                }
+            }
+
+            // Проверка 3x3 квадрата
+            int startRow = (row / 3) * 3;
+            int startCol = (col / 3) * 3;
+
+            for (int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startCol; j < startCol + 3; j++)
+                {
+                    if (_board[i, j] == value)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void DisplayBoard(int selectedRow, int selectedCol)
+        {
+            Console.WriteLine("Доска судоку:");
+            for (int i = 0; i < 9; i++)
+            {
+                if (i % 3 == 0 && i != 0)
+                {
+                    Console.WriteLine("-------------------------------");
+                }
+
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j % 3 == 0 && j != 0)
+                    {
+                        Console.Write("| ");
+                    }
+
+                    if (i == selectedRow && j == selectedCol)
+                    {
+                        Console.Write($"[{(_board[i, j] != 0 ? _board[i, j].ToString() : ".")}] ");
+                    }
+                    else
+                    {
+                        Console.Write(_board[i, j] != 0 ? _board[i, j].ToString() : ". ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,14 +4,15 @@
     {
         static void Main()
         {
-            int[,] board = new int[9, 9];
+            Board board = new Board();
+
             int selectedRow = -1;
             int selectedCol = -1;
 
             while (true)
             {
                 Console.Clear();
-                DisplayBoard(board, selectedRow, selectedCol);
+                board.DisplayBoard(selectedRow, selectedCol);
                 Console.WriteLine("Выберите действие:");
                 Console.WriteLine("1. Выбрать клетку");
                 Console.WriteLine("2. Заполнить ячейку");
@@ -32,11 +33,7 @@
                         Console.Write("Введите значение (1-9): ");
                         int value = int.Parse(Console.ReadLine());
 
-                        if (IsValidMove(board, selectedRow, selectedCol, value))
-                        {
-                            board[selectedRow, selectedCol] = value;
-                        }
-                        else
+                        if (!board.InsertValue(selectedRow, selectedCol, value))
                         {
                             Console.WriteLine("Недопустимый ход. Пожалуйста, проверьте правила судоку.");
                             Console.ReadLine();
@@ -54,65 +51,6 @@
                     break;
                 }
             }
-        }
-
-        static void DisplayBoard(int[,] board, int selectedRow, int selectedCol)
-        {
-            Console.WriteLine("Доска судоку:");
-            for (int i = 0; i < 9; i++)
-            {
-                if (i % 3 == 0 && i != 0)
-                {
-                    Console.WriteLine("-------------------------------");
-                }
-
-                for (int j = 0; j < 9; j++)
-                {
-                    if (j % 3 == 0 && j != 0)
-                    {
-                        Console.Write("| ");
-                    }
-
-                    if (i == selectedRow && j == selectedCol)
-                    {
-                        Console.Write($"[{(board[i, j] != 0 ? board[i, j].ToString() : ".")}] ");
-                    }
-                    else
-                    {
-                        Console.Write(board[i, j] != 0 ? board[i, j].ToString() : ". ");
-                    }
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static bool IsValidMove(int[,] board, int row, int col, int value)
-        {
-            // Проверка строки и столбца
-            for (int i = 0; i < 9; i++)
-            {
-                if (board[row, i] == value || board[i, col] == value)
-                {
-                    return false;
-                }
-            }
-
-            // Проверка 3x3 квадрата
-            int startRow = (row / 3) * 3;
-            int startCol = (col / 3) * 3;
-
-            for (int i = startRow; i < startRow + 3; i++)
-            {
-                for (int j = startCol; j < startCol + 3; j++)
-                {
-                    if (board[i, j] == value)
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
         }
     }
 }
